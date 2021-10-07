866a6fe427805ffe7e7cc977e2ba574330b422eb
Matheus Lima <l.matheus08@outlook.com>
2021-10-07 08:42:12 -0400

    Initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,70 +1,108 @@
-import React from 'react';
-import { 
-    View, 
-    Text, 
-    StyleSheet, 
-    TextInput, 
-    Platform,
-    TouchableOpacity
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity
 } from 'react-native';
 
 export function Home() {
-  return(
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
+  return (
     <View style={styles.container}>
       <Text style={styles.title}>
-          Welcome, Matheus
-        </Text>
+        Welcome, Matheus
+      </Text>
 
-      <TextInput 
+      <TextInput
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
+        onChangeText={setNewSkill}
       />
-      
-      <TouchableOpacity 
-            style={styles.button}
-            activeOpacity={.7}
-            >
-          <Text style={styles.buttonText}>Add</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={.7}
+        onPress={handleAddNewSkill}
+      >
+        <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
-      <Text style={[ styles.title, {marginTop: 50 }]}>My skill</Text>
+      <Text style={[styles.title, { marginVertical: 50 }]}>
+        My Skill
+      </Text>
+
+      {
+        mySkills.map(skill => (
+          <TouchableOpacity
+            key={skill}
+            style={styles.buttonSkill}
+            activeOpacity={.7}
+          >
+            <Text style={styles.textSkill}>
+              {skill}
+            </Text>
+          </TouchableOpacity>
+        ))
+      }
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#121015',
-        paddingVertical: 70,
-        paddingHorizontal: 30
-    },
-    title: {
-        color: '#FFF',
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    input: {
-        backgroundColor: '#1F1E25',
-        color: '#FFF',
-        fontSize: 18,
-        padding: Platform.OS === 'ios' ? 15 : 10,
-        marginTop: 30,
-        borderRadius: 7
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
 
-    },
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,
-        alignItems: 'center',
-        marginTop: 20
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 17,
-        fontWeight: 'bold'
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold'
 
-    }
+  },
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  textSkill: {
+    color: '#FFF',
+    fontSize: 22,
+    fontWeight: 'bold',
+  }
 })
